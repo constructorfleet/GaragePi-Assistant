44beac7e2f8fda62ffe78978ff15deff1da266b7
Teagan glenn <teagan.m.glenn@gmail.com>
2020-05-17 21:28:33 -0600

    Ensure types

--- garagepi/domain/models/GarageDoor.py
+++ garagepi/domain/models/GarageDoor.py
@@ -19,43 +19,52 @@ class GarageDoor:
     """Class representing a garage door instance."""
 
     in_motion = False
-    position = 0
-    prev_position = 0
+    _position = 0
+    _prev_position = 0
 
     def __init__(self, entity_id, initial_position):
         self.entity_id = entity_id
-        self.position = self.prev_position = initial_position
+        self._position = self._prev_position = initial_position
 
     @property
     def is_closed(self):
-        return self.position == 0
+        return self._position == 0
 
     @property
     def is_open(self):
-        return self.position == 100
+        return self._position == 100
+
+    @property
+    def position(self):
+        return int(self._position)
+
+    @property
+    def prev_position(self):
+        return int(self._prev_position)
 
     @property
     def state(self):
         if not self.in_motion \
-                and self.position not in [0, 100]:
+                and self._position not in [0, 100]:
             return DoorState.STOPPED.value
-        if self.position == self.prev_position:
-            if self.position == 0:
+        if self._position == self._prev_position:
+            self.in_motion = False
+            if self._position == 0:
                 return DoorState.CLOSED.value
-            if 100 == self.position:
+            if 100 == self._position:
                 return DoorState.OPEN.value
-        if self.position > self.prev_position and self.in_motion:
+        if self._position > self._prev_position and self.in_motion:
             return DoorState.OPENING.value
-        if self.position < self.prev_position and self.in_motion:
+        if self._position < self._prev_position and self.in_motion:
             return DoorState.CLOSING.value
 
     @property
     def attributes(self):
         return {
-            ATTR_POSITION: self.position
+            ATTR_POSITION: self._position
         }
 
     def set_position(self, position):
-        self.prev_position = self.position
-        self.position = position
-        self.in_motion = self.position != self.prev_position
+        self._prev_position = self._position
+        self._position = position
+        self.in_motion = self._position != self._prev_position
